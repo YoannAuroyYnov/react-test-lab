59cadf732425e74999266e8ec7b1f62271b9cbf9
Yoann Auroy <yoann.ar.pro@gmail.com>
2026-02-18 16:22:31 +0100

    install cypress

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+module.exports = defineConfig({
+  allowCypressEnv: false,
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
